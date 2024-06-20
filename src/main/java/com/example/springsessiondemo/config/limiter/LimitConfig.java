bf36a507126a42b25cec09cd750a3213bfc6d84c
丁亚宾 <1262027459@qq.com>
2024-06-20 14:46:41 +0800

    add limiter

--- src/main/java/com/example/springsessiondemo/config/limiter/LimitConfig.java
+++ src/main/java/com/example/springsessiondemo/config/limiter/LimitConfig.java
@@ -1,6 +1,5 @@
 package com.example.springsessiondemo.config.limiter;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
