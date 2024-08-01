db1c08e57edcc7c8369a3617ee8e7f8b1ee8248c
丁亚宾 <1262027459@qq.com>
2024-08-02 00:01:00 +0800

    修改端口

--- sa-token-demo/src/main/java/com/dingyabin/satoken/SaTokenDemoApplication.java
+++ sa-token-demo/src/main/java/com/dingyabin/satoken/SaTokenDemoApplication.java
@@ -1,15 +1,15 @@
-package com.dingyabin.springsecuritydemo;
+package com.dingyabin.satoken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 // 优质的 spring/boot/data/security/cloud 框架中文文档尽在 => https://springdoc.cn
-@SpringBootApplication(scanBasePackages = {"com.dingyabin.springsecuritydemo", "com.dingyabin.security"})
-public class SpringSecurityDemoApplication {
+@SpringBootApplication(scanBasePackages = {"com.dingyabin.satoken", "com.dingyabin.security"})
+public class SaTokenDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+        SpringApplication.run(SaTokenDemoApplication.class, args);
     }
 
 }
---
+++ security-data-common/src/main/java/com/dingyabin/security/config/MybatisConfig.java
@@ -0,0 +1,16 @@
+package com.dingyabin.security.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 丁亚宾
+ * Date: 2024/8/1.
+ * Time:23:57
+ */
+@Configuration
+@MapperScan("com.dingyabin.security.mapper")
+public class MybatisConfig {
+
+}
+
