26716cc07e47da9ea0b291d3e04bb39f85927a4c
丁亚宾 <1262027459@qq.com>
2024-11-05 18:18:14 +0800

    spring-resilience4j

--- spring-resilience4j-demo/src/main/java/com/dingyabin/resilience4j/controller/ResilienceController.java
+++ spring-resilience4j-demo/src/main/java/com/dingyabin/resilience4j/controller/ResilienceController.java
@@ -2,10 +2,15 @@ package com.dingyabin.resilience4j.controller;
 
 import com.dingyabin.response.Result;
 import com.dingyabin.web.common.BaseController;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.RetryRegistry;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author 丁亚宾
@@ -15,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ResilienceController extends BaseController {
 
+    @Resource
+    private RateLimiterRegistry rateLimiterRegistry;
+
+    @Resource
+    private RetryRegistry retryRegistry;
+
 
     @RequestMapping("/testRateLimiter")
     @RateLimiter(name = "controllerA", fallbackMethod = FALLBACK_METHOD_NAME)
@@ -23,4 +34,19 @@ public class ResilienceController extends BaseController {
     }
 
 
+    @RequestMapping("/testRateLimiter2")
+    public Result<String> testRateLimiter2() {
+        if (rateLimiterRegistry.rateLimiter("controllerA").acquirePermission()) {
+            return Result.success("ok");
+        }
+        return Result.fail(100, "稍后再试...");
+    }
+
+
+    @Retry(name = "serviceA")
+    @RequestMapping("/testRetry")
+    public Result<String> testRetry() {
+        System.out.println("开始执行");
+        throw new RuntimeException("失败了......");
+    }
 }
