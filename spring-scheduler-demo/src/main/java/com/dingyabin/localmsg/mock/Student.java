65b490154129d2a0a6e907a1d7279455a6aa3847
dingyabin <1262027459@qq.com>
2025-06-19 00:26:12 +0800

    fix

--- spring-scheduler-demo/src/main/java/com/dingyabin/localmsg/mock/Student.java
+++ spring-scheduler-demo/src/main/java/com/dingyabin/localmsg/mock/Student.java
@@ -1,11 +1,14 @@
 package com.dingyabin.localmsg.mock;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Student {
 
     private Integer age;
