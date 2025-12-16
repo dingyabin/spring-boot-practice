3ad584ee35fe1b26c01b73553a9c41fa76d5ef3b
dingyabin <1262027459@qq.com>
2025-12-16 22:14:53 +0800

    修改version3

---
+++ spring-scheduler-demo/src/main/java/com/dingyabin/scheduler/config/SchedulerLockConfig.java
@@ -0,0 +1,27 @@
+package com.dingyabin.scheduler.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "30s") // 默认最长锁 30 秒
+public class SchedulerLockConfig {
+
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime() // 使用数据库时间（推荐，避免节点时间不同步）
+                        .build()
+        );
+    }
+
+}
--- spring-scheduler-demo/src/main/java/com/dingyabin/scheduler/task/SpringAnnotationSchedulerTask.java
+++ spring-scheduler-demo/src/main/java/com/dingyabin/scheduler/task/SpringAnnotationSchedulerTask.java
@@ -1,5 +1,6 @@
 package com.dingyabin.scheduler.task;
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,8 @@ import java.time.LocalTime;
 @Component
 public class SpringAnnotationSchedulerTask {
 
-    //@Scheduled(fixedDelay = 2000)
+    @Scheduled(cron = "*/10 * * * * ?")
+    @SchedulerLock(name = "springAnnotationSchedulerTask.execute", lockAtLeastFor = "5s", lockAtMostFor = "10s")
     public void execute(){
         System.out.println(">>>>>>>>>>>>>>这是Spring自带的@Scheduled注解的任务: "+ Thread.currentThread().getName()+ " " + LocalTime.now());
     }
