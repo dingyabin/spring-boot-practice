22c2d4afd566e3ec49b5fc7ed461e73081f56cfa
dingyabin <1262027459@qq.com>
2026-08-12 21:41:12 +0800

    fix(conversion): 修复fix

--- spring-web-common/src/main/java/com/dingyabin/web/conversion/config/ConversionConfig.java
+++ spring-web-common/src/main/java/com/dingyabin/web/conversion/config/ConversionConfig.java
@@ -1,11 +1,16 @@
 package com.dingyabin.web.conversion.config;
 
 import cn.hutool.core.collection.CollUtil;
+import com.dingyabin.web.conversion.annotation.Conversion;
 import com.dingyabin.web.conversion.annotation.ConversionType;
 import com.dingyabin.web.conversion.core.ConversionInterface;
-import com.dingyabin.web.conversion.core.handler.ConversionBeanSerializerModifier;
 import com.dingyabin.web.conversion.core.handler.ConversionSerializer;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
@@ -13,9 +18,7 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -40,9 +43,29 @@ public class ConversionConfig {
             }
         }
         //注册BeanSerializerModifier
-        SimpleModule simpleModule = new SimpleModule();
-        simpleModule.setSerializerModifier(new ConversionBeanSerializerModifier());
+        SimpleModule simpleModule = buildSimpleModule();
         return builder -> builder.modulesToInstall(modules -> modules.add(simpleModule));
+    }
+
+
+
+
+    private SimpleModule buildSimpleModule() {
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.setSerializerModifier(new BeanSerializerModifier() {
+            @Override
+            public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription beanDesc, List<BeanPropertyWriter> beanProperties) {
+                for (BeanPropertyWriter writer : beanProperties) {
+                    // 如果序列化器为 ConversionSerializer 的话 将 Null 值也交给他处理
+                    JsonSerializer<Object> serializer = writer.getSerializer();
+                    if (serializer instanceof ConversionSerializer || writer.getAnnotation(Conversion.class) != null) {
+                        writer.assignNullSerializer(serializer);
+                    }
+                }
+                return beanProperties;
+            }
+        });
+        return simpleModule;
     }
 
 }
